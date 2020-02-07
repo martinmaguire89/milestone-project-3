4ee7e39101a86d2ef60f26a4b3479901c7fe8751
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-02-07 20:06:04 +0000

    sign up page added and linked with log in page

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
