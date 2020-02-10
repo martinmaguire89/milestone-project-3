54a4f1c9ba35548a67a9e1ed9ddaa6f98d4a02a6
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-02-10 20:12:55 +0000

    pymongo(app) added

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, request
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
