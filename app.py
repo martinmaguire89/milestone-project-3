ca5257174f474a651fda901c35a6a5ae5ce5cb83
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-02-10 19:33:11 +0000

    make better

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def signup():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT', '5000')),
+            port=int(os.environ.get('PORT')),
             debug=True)
