44309960a4d4a59ec0293b0cd9bef2b3cdd1d3d5
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-02-10 20:07:40 +0000

    attempts to fix bug with pymongo

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, request
-from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
