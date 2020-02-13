3c4173af8586e331a95354369ad4a8309a679e21
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-02-13 19:12:26 +0000

    adding env.py to hide login details and password

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://martin-maguire:Noucamp99@myfirstcluster-doje8.mongodb.net/greatest_fighters?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-
+@app.route('/index')
 def index():
     return render_template("index.html")
 
---
+++ env.py
@@ -0,0 +1,2 @@
+import os
+os.environ['MONGO_URI'] = 'mongodb+srv://martin-maguire:Noucamp99@myfirstcluster-doje8.mongodb.net/greatest_fighters?retryWrites=true&w=majority'
