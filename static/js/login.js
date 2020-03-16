a29aa8bc33b4695175ae00d374adb1f6a2cc9ccb
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-03-16 15:42:58 +0000

    updated edit fighter

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,4 @@
-///$('.message a').click(function(){
-  /// $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 
