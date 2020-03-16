3fc01c72a91386f5a13d4a42e6d9fff0b9706184
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-03-16 12:06:26 +0000

    date pciker add

--- static/js/login.js
+++ static/js/login.js
@@ -1,11 +1,4 @@
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+///$('.message a').click(function(){
+  /// $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 
-$('.datepicker').pickadate({
-// Escape any “rule” characters with an exclamation mark (!).
-format: 'You selecte!d: dddd, dd mmm, yyyy',
-formatSubmit: 'yyyy/mm/dd',
-hiddenPrefix: 'prefix__',
-hiddenSuffix: '__suffix'
-})
